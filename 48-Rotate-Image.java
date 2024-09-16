69d371ae0137d8c049957addfaff574f674b74cd
JaydeepKhandla <jaydeepkhandla01@gmail.com>
2024-09-16 09:46:55 +0530

    48-Rotate-Image

---
+++ 48-Rotate-Image.java
@@ -0,0 +1,57 @@
+public class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                
+                left++;
+                right--;
+            }
+        }
+    }
+
+    // Utility function to print the matrix
+    private void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        
+        System.out.println("Original matrix:");
+        sol.printMatrix(matrix);
+        
+        sol.rotate(matrix);
+        
+        System.out.println("Rotated matrix:");
+        sol.printMatrix(matrix);
+    }
+}
