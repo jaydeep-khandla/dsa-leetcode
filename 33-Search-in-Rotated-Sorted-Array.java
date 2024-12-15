bbdb5101ccbd6efd337d0c8dbd197d2b0411ea45
JaydeepKhandla <jaydeepkhandla01@gmail.com>
2024-12-15 12:44:47 +0530

    33-Search-in-Rotated-Sorted-Array

---
+++ 33-Search-in-Rotated-Sorted-Array.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[start] <= nums[mid]){
+                if (target >= nums[start] && target <= nums[mid]){
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target >= nums[mid] && target <= nums[end]){
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        int[] test1 = {4,5,6,7,0,1,2};
+        int target1 = 0;
+        int[] test2 = {4,5,6,7,0,1,2};
+        int target2 = 3;
+        int[] test3 = {1};
+        int target3 = 0;
+        int[] test4 = {1,3};
+        int target4 = 3;
+
+        System.out.println(sol.search(test1, target1)); // Expected: 4
+        System.out.println(sol.search(test2, target2)); // Expected: -1
+        System.out.println(sol.search(test3, target3)); // Expected: -1
+        System.out.println(sol.search(test4, target4)); // Expected: 1
+        
+    }
+}
